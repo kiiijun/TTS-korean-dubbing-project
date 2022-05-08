3e99aafba2166f6aef5d8db696bf37e7109cebfd
kiiijun <snrm123@naver.com>
2022-05-09 00:36:23 +0900

    fix source file

--- src/videoDubbing.py
+++ src/videoDubbing.py
@@ -2,6 +2,7 @@
 from moviepy.editor import *   #import moviepy
 videoclip = VideoFileClip("movie.mp4") #video clip
 audioclip = AudioFileClip("dubbing.mp3") #audio clip
-new_audioclip = CompositeAudioClip([audioclip]) #Composite
+audioclip2 = AudioFileClip("background.mp3") #background audioclip
+new_audioclip = CompositeAudioClip([audioclip, audioclip2]) #Composite audioclip and audioclip2
 videoclip.audio = new_audioclip
 videoclip.write_videofile("result.mp4")
